ba6110e0892481d44d6b0d4608f0fbe7088111d0
BatuhanGoktaas <quackhard@gmail.com>
2021-12-04 01:49:13 +0300

    List added

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "./Colors";
+import tempData from "./tempData";
+import TodoList from "./components/TodoList";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Başlangıç</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={{ flexDirection: "row" }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Plan <Text style={{ fontWeight: "300", color: colors.blue }}>Rehberim</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name="plus" size={16} color={colors.blue} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Plan Ekle</Text>
+        </View>
+        <View style={{ height: 275, paddingLeft: 32 }}>
+          <FlatList
+            data={tempData}
+            keyExtractor={(item) => item.name}
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => <TodoList list={item} />}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f3ffa3',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  divider: {
+    backgroundColor: colors.lightBlue,
+    height: 1,
+    flex: 1,
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 46,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightBlue,
+    borderRadius: 4,
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  add: {
+    color: colors.blue,
+    fontWeight: "600",
+    fontSize: 14,
+    marginTop: 8,
   },
 });
---
+++ tempData.js
@@ -0,0 +1,64 @@
+export default tempData = [
+  {
+    name: "Yolculuk Planlaması",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Kitap Okunacak",
+        completed: false,
+      },
+      {
+        title: "Müzik Dinlenecek",
+        completed: true,
+      },
+      {
+        title: "Hediyelik Eşya Alınacak",
+        completed: true,
+      },
+      {
+        title: "Meyve Yenilecek",
+        completed: false,
+      },
+      {
+        title: "Süt Alınacak",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Doğum Günü Partisi",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Süt Alınacak",
+        completed: true,
+      },
+      {
+        title: "Hediye Seçilecek",
+        completed: true,
+      },
+      {
+        title: "Herkes aranacak",
+        completed: true,
+      },
+      {
+        title: "Meyve Yenilecek",
+        completed: false,
+      },
+    ],
+  },
+  {
+    name: "Eşşek Abbas Filmi",
+    color: "#595BD9",
+    todos: [
+      {
+        title: "Foto çekilecek",
+        completed: true,
+      },
+      {
+        title: "Arkadaş edinilecek",
+        completed: true,
+      },
+    ],
+  },
+];
